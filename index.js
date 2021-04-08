501538f299e38dee8889b704fafe023f9821a157
Romero Sierra <rsinclaire@protonmail.com>
2021-04-08 06:58:22 -0400

    added index.html and about.html

--- index.js
+++ index.js
@@ -5,9 +5,29 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     if (req.url === '/') {
-        res.end('<h1>Homepage</h1>');
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err; 
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.end(content);
+            }
+        );
+    }
+
+    if (req.url === '/about') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+            (err, content) => {
+                if (err) throw err; 
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.end(content);
+            }
+        );
     }
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 
