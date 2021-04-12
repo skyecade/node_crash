f882e13200766d486581f48b3e5e4f53070bc04d
Romero Sierra <rsinclaire@protonmail.com>
2021-04-12 19:57:30 -0400

    added 404 page and created basic server

--- index.js
+++ index.js
@@ -3,28 +3,72 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    if (req.url === '/') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
-            (err, content) => {
-                if (err) throw err; 
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end(content);
-            }
-        );
+    // if (req.url === '/') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'index.html'),
+    //         (err, content) => {
+    //             if (err) throw err; 
+    //             res.writeHead(200, { 'Content-Type': 'text/html'});
+    //             res.end(content);
+    //         }
+    //     );
+    // }
+
+    // if (req.url === '/about') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'about.html'),
+    //         (err, content) => {
+    //             if (err) throw err; 
+    //             res.writeHead(200, { 'Content-Type': 'text/html'});
+    //             res.end(content);
+    //         }
+    //     );
+    // }
+
+    //     if (req.url === '/api/users') {
+    //     const users = [
+    //         { name: 'Bob Sinclaire', age: 47 },
+    //         { name: 'Meagan Leard', age: 44 }
+    //     ];
+    //     res.writeHead(200, { 'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 
+    'index.html' : req.url);
+
+    // Get exttension of file
+    let extname = path.extname(filePath);
+
+    // Initial conent type
+    let contentType = 'text/hthl';
+
+    //Check extention and set content type
+    switch(extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
 
-    if (req.url === '/about') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'about.html'),
-            (err, content) => {
-                if (err) throw err; 
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end(content);
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code == 'ENOENT') {
+                // Page not found
+                
             }
-        );
-    }
+        }
+    });
 });
 
 
