fb1108d08d9f10e8849261d02bd5b89632dc3fda
Robert Sinclaire <skyecade@gmail.com>
2021-04-21 16:01:09 -0400

    edited sample

--- sample.js
+++ sample.js
@@ -3,36 +3,11 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url);
      if (req.url === '/') {
-         fs.readFile(
-             path.join(__dirname, 'public', 'index.html'),
-             (err, content) => {
-                 if (err) throw err; 
-                 res.writeHead(200, { 'Content-Type': 'text/html'});
-                 res.end(content);
-             }
-         );
+         res.end('<h1>Hello World</h1>')
      }
-
-     if (req.url === '/about') {
-         fs.readFile(
-             path.join(__dirname, 'public', 'about.html'),
-             (err, content) => {
-                 if (err) throw err; 
-                 res.writeHead(200, { 'Content-Type': 'text/html'});
-                 res.end(content);
-             }
-         );
-     }
-
-         if (req.url === '/api/users') {
-         const users = [
-             { name: 'Bob Sinclaire', age: 47 },
-             { name: 'Meagan Leard', age: 44 }
-         ];
-         res.writeHead(200, { 'Content-Type': 'application/json'});
-         res.end(JSON.stringify(users));
-     }  
+       
 });
 
 const PORT = process.env.PORT || 5000;
